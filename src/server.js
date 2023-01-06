26d39723fad54ab6b47fd5ede64c6834865bc78d
GeorgeMc1 <george.man1@hotmail.co.uk>
2023-01-06 10:34:40 +0000

    ready for deployment

--- src/server.js
+++ src/server.js
@@ -12,6 +12,11 @@ app.use(express.json());
 app.use(userRouter);
 app.use(movieRouter);
 
+//endpoint to check if the API is working for deployment
+app.get("/health", (req, res) => {
+    res.status(200).send({message: "API is working"});
+})
+
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 })
